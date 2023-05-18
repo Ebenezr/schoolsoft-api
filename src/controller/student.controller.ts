5d646af8bc10b2c9054c688ec5dd215beba949bf
Ebenezr <ebenezarbukosia@gmail.com>
2023-05-18 10:34:51 +0300

    ref

--- src/controller/student.controller.ts
+++ src/controller/student.controller.ts
@@ -18,9 +18,42 @@ router.post(
   }
 );
 
-// get all students
+// get all students with pagination
 router.get(
   "/students",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const page = parseInt(req.query.page as string, 10) || 1;
+      const limit = parseInt(req.query.limit as string, 10) || 10;
+      const startIndex = (page - 1) * limit;
+      const endIndex = startIndex + limit;
+
+      const students = await prisma.student.findMany({
+        orderBy: {
+          createdAt: "desc",
+        },
+        skip: startIndex,
+        take: limit,
+      });
+
+      const totalItems = await prisma.student.count();
+
+      res.status(200).json({
+        currentPage: page,
+        totalPages: Math.ceil(totalItems / limit),
+        itemsPerPage: limit,
+        totalItems: totalItems,
+        items: students.slice(0, endIndex),
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// get all students
+router.get(
+  "/students/all",
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       const students = await prisma.student.findMany();
