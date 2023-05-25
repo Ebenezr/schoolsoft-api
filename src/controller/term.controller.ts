1db400814941f3388d371210cb9e4dbe5506483b
Ebenezr <ebenezarbukosia@gmail.com>
2023-05-25 21:59:04 +0300

    ref

--- src/controller/term.controller.ts
+++ src/controller/term.controller.ts
@@ -52,10 +52,6 @@ router.get(
         orderBy: {
           createdAt: "desc",
         },
-        // return student's class name
-        include: {
-          Class: true,
-        },
         skip: startIndex,
         take: limit,
       });
@@ -134,73 +130,6 @@ router.delete(
     } catch (error) {
       next(error);
       return res.status(404).json({ message: "term not found" });
-    }
-  }
-);
-
-router.get(
-  "/terms/search/:name",
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { name } = req.params;
-    try {
-      const page = parseInt(req.query.page as string, 10) || 1;
-      const limit = parseInt(req.query.limit as string, 10) || 10;
-      const startIndex = (page - 1) * limit;
-      const endIndex = startIndex + limit;
-
-      const terms = await prisma.term.findMany({
-        where: {
-          OR: [
-            {
-              first_name: {
-                contains: name?.toString().toLowerCase() || "",
-                mode: "insensitive",
-              },
-            },
-            {
-              last_name: {
-                contains: name?.toString().toLowerCase() || "",
-                mode: "insensitive",
-              },
-            },
-          ],
-        },
-        skip: startIndex,
-        take: limit,
-      });
-
-      if (!terms) {
-        return res.status(404).json({ error: "term not found" });
-      }
-
-      const totalItems = await prisma.term.count({
-        where: {
-          OR: [
-            {
-              first_name: {
-                contains: name?.toString().toLowerCase() || "",
-                mode: "insensitive",
-              },
-            },
-            {
-              last_name: {
-                contains: name?.toString().toLowerCase() || "",
-                mode: "insensitive",
-              },
-            },
-          ],
-        },
-      });
-
-      res.status(200).json({
-        currentPage: page,
-        totalPages: Math.ceil(totalItems / limit),
-        itemsPerPage: limit,
-        totalItems: totalItems,
-        items: terms.slice(0, endIndex),
-      });
-    } catch (error: any) {
-      next(error);
     }
   }
 );
