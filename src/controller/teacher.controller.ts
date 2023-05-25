d4f9103b4e0ea793529bd28d822cb0ce0e1e85dd
Ebenezr <ebenezarbukosia@gmail.com>
2023-05-25 10:59:41 +0300

    minor fixes

--- src/controller/teacher.controller.ts
+++ src/controller/teacher.controller.ts
@@ -7,96 +7,160 @@ const router = Router();
 // post new teacher
 
 router.post(
-    "/teacher/post",
-    async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const data = req.body;
-            const teacher = await prisma.teacher.create({ data });
-            res.status(201).json (teacher);
-        } catch (error) {
-            next(error);
-        }
+  "/teacher/post",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body;
+      const teacher = await prisma.teacher.create({ data });
+      res.status(201).json(teacher);
+    } catch (error) {
+      next(error);
     }
-    
-    );
+  }
+);
 
-    //  get all teachers
+//  get all teachers
 
-    router.get(
-        "/teachers",
-        async (req: Request, res: Response, next: NextFunction) => {
-            try {
-              const teachers = await prisma.teacher.findMany();
-              res.status(200).json(teachers);
-            } catch (error) {
-              next(error);
-            }
-          }
-        );
+router.get(
+  "/teachers",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const teachers = await prisma.teacher.findMany();
+      res.status(200).json(teachers);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-    // get one teacher
-    router.get(
-        "/teacher/:id",
-        async (req: Request, res: Response, next: NextFunction) => {
-            try {
-              const { id } = req.params;
-              const teacher = await prisma.teacher.findUnique({
-                where: {
-                  id: Number(id),
-                },
-              });
-              if (!teacher) {
-                return res.status(404).json({ message: "Student not found" });
-              }
-              res.status(200).json(teacher);
-            } catch (error) {
-              next(error);
-            }
-          }
-        );
-    
-        // update teacher
-        router.patch(
-            "/teacher/:id",
-            async (req: Request, res: Response, next: NextFunction) => {
-              try {
-                const { id } = req.params;
-                const teacher = await prisma.teacher.update({
-                  where: {
-                    id: Number(id),
-                  },
-                  data: req.body,
-                });
-          
-                res.status(202).json(teacher);
-              } catch (error) {
-                next(error);
-                return res.status(404).json({ message: "Teacher not found" });
-              }
-            }
-          );
+// get one teacher
+router.get(
+  "/teacher/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      const teacher = await prisma.teacher.findUnique({
+        where: {
+          id: Number(id),
+        },
+      });
+      if (!teacher) {
+        return res.status(404).json({ message: "Student not found" });
+      }
+      res.status(200).json(teacher);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-        //   delete student
+// update teacher
+router.patch(
+  "/teacher/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      const teacher = await prisma.teacher.update({
+        where: {
+          id: Number(id),
+        },
+        data: req.body,
+      });
 
-        router.delete(
-            "/teacher/:id",
-            async (req: Request, res: Response, next: NextFunction) => {
-              try {
-                const { id } = req.params;
-                const teacher = await prisma.teacher.delete({
-                  where: {
-                    id: Number(id),
-                  },
-                });
-          
-                res.status(204).json(teacher);
-              } catch (error) {
-                next(error);
-                return res.status(404).json({ message: "Teacher not found" });
-              }
-            }
-          );
-          
+      res.status(202).json(teacher);
+    } catch (error) {
+      next(error);
+      return res.status(404).json({ message: "Teacher not found" });
+    }
+  }
+);
 
+//   delete student
+
+router.delete(
+  "/teacher/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      const teacher = await prisma.teacher.delete({
+        where: {
+          id: Number(id),
+        },
+      });
+
+      res.status(204).json(teacher);
+    } catch (error) {
+      next(error);
+      return res.status(404).json({ message: "Teacher not found" });
+    }
+  }
+);
+
+router.get(
+  "/teachers/search/:name",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { name } = req.params;
+    try {
+      const page = parseInt(req.query.page as string, 10) || 1;
+      const limit = parseInt(req.query.limit as string, 10) || 10;
+      const startIndex = (page - 1) * limit;
+      const endIndex = startIndex + limit;
+
+      const teachers = await prisma.teacher.findMany({
+        where: {
+          OR: [
+            {
+              first_name: {
+                contains: name?.toString().toLowerCase() || "",
+                mode: "insensitive",
+              },
+            },
+            {
+              last_name: {
+                contains: name?.toString().toLowerCase() || "",
+                mode: "insensitive",
+              },
+            },
+          ],
+        },
+        skip: startIndex,
+        take: limit,
+      });
+
+      if (!teachers) {
+        return res.status(404).json({ error: "Teacher not found" });
+      }
+
+      const totalItems = await prisma.teacher.count({
+        where: {
+          OR: [
+            {
+              first_name: {
+                contains: name?.toString().toLowerCase() || "",
+                mode: "insensitive",
+              },
+            },
+            {
+              last_name: {
+                contains: name?.toString().toLowerCase() || "",
+                mode: "insensitive",
+              },
+            },
+          ],
+        },
+      });
+
+      res.status(200).json({
+        currentPage: page,
+        totalPages: Math.ceil(totalItems / limit),
+        itemsPerPage: limit,
+        totalItems: totalItems,
+        items: teachers.slice(0, endIndex),
+      });
+    } catch (error: any) {
+      next(error);
+    }
+  }
+);
 
 export default router;
