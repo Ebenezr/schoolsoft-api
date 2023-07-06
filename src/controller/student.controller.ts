3fc9ab290cf923fd6c91e3d7c55bbc9260953464
ClementNjeru <njeruclement36@gmail.com>
2023-07-06 15:19:49 +0300

    refactor code

--- src/controller/student.controller.ts
+++ src/controller/student.controller.ts
@@ -11,107 +11,8 @@ router.post(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       const data = req.body;
-      const additionalPayments = req.body.additionalPayments;
-
-      // Create the student record
-      const student = await prisma.student.create({
-        data: {
-          first_name: data.first_name,
-          last_name: data.last_name,
-          dob: data.dob,
-          gender: data.gender,
-          classId: data.classId,
-        },
-      });
-
-      // Get the class information
-      const classInfo = await prisma.class.findUnique({
-        where: {
-          id: data.classId,
-        },
-      });
-
-      if (!classInfo) {
-        throw new Error('Class not found');
-      }
-
-      // Find additional fees from the database based on regex patterns
-      const fees = await prisma.additionalFee.findMany({
-        where: {
-          OR: [
-            { name: { contains: 'bus', mode: 'insensitive' } },
-            { name: { contains: 'food', mode: 'insensitive' } },
-            { name: { contains: 'boarding', mode: 'insensitive' } },
-          ],
-        },
-      });
-
-      // Create additional fee student records based on matched fees
-      const additionalFeeStudents = fees.map((fee) => {
-        return prisma.additionalFeeStudent.create({
-          data: {
-            studentId: student.id,
-            additionalFeeId: fee.id,
-          },
-        });
-      });
-
-      // Wait for all additional fee student records to be created
-      await Promise.all(additionalFeeStudents);
-
-      // Calculate the fee amounts
-      let busFee = 0;
-      let foodFee = 0;
-      let boardingFee = 0;
-
-      for (const fee of fees) {
-        if (fee.name.toLowerCase().includes('bus')) {
-          busFee += Number(fee.amount);
-        }
-        if (fee.name.toLowerCase().includes('food')) {
-          foodFee += Number(fee.amount);
-        }
-        if (fee.name.toLowerCase().includes('boarding')) {
-          boardingFee += Number(fee.amount);
-        }
-      }
-
-      // Create the student term fee record
-      const studentTermFee = await prisma.studentTermFee.create({
-        data: {
-          studentId: student.id,
-          classId: data.classId,
-          bus_fee: additionalPayments.bus_fee ? busFee : 0,
-          food_fee: additionalPayments.food_fee ? foodFee : 0,
-          boarding_fee: additionalPayments.boarding_fee ? boardingFee : 0,
-          term_one_fee: classInfo.term_1 ?? 0,
-          term_one_paid: additionalPayments.term_one_paid ?? 0,
-          term_one_balance:
-            Number(classInfo.term_1) +
-              (additionalPayments.bus_fee ? busFee : 0) +
-              (additionalPayments.food_fee ? foodFee : 0) +
-              (additionalPayments.boarding_fee ? boardingFee : 0) -
-              (additionalPayments.term_one_paid ?? 0) ?? 0,
-          term_two_fee: classInfo.term_2 ?? 0,
-          term_two_paid: additionalPayments.term_two_paid ?? 0,
-          term_two_balance:
-            Number(classInfo.term_2) +
-              (additionalPayments.bus_fee ? busFee : 0) +
-              (additionalPayments.food_fee ? foodFee : 0) +
-              (additionalPayments.boarding_fee ? boardingFee : 0) -
-              (additionalPayments.term_two_paid ?? 0) ?? 0,
-          term_three_fee: classInfo.term_3 ?? 0,
-          term_three_paid: additionalPayments.term_three_paid ?? 0,
-          term_three_balance:
-            Number(classInfo.term_3) +
-              (additionalPayments.bus_fee ? busFee : 0) +
-              (additionalPayments.food_fee ? foodFee : 0) +
-              (additionalPayments.boarding_fee ? boardingFee : 0) -
-              (additionalPayments.term_three_paid ?? 0) ?? 0,
-        },
-      });
-
-      res.status(201).json({ student, studentTermFee });
+      const student = await prisma.student.create({ data });
+      res.status(201).json(student);
     } catch (error) {
       next(error);
     }
@@ -125,137 +26,21 @@ router.patch(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { id } = req.params;
-      const data = req.body;
-      const additionalPayments = req.body.additionalPayments;
-
-      // Update the student record
       const student = await prisma.student.update({
-        where: { id: Number(id) },
-        data: {
-          first_name: data.first_name,
-          last_name: data.last_name,
-          dob: data.dob,
-          gender: data.gender,
-          classId: data.classId,
-        },
-      });
-
-      // Get the class information
-      const classInfo = await prisma.class.findUnique({
         where: {
-          id: data.classId,
+          id: Number(id),
         },
+        data: req.body,
       });
-
-      if (!classInfo) {
-        throw new Error('Class not found');
-      }
-
-      // Find additional fees from the database based on regex patterns
-      const fees = await prisma.additionalFee.findMany({
-        where: {
-          OR: [
-            { name: { contains: 'bus', mode: 'insensitive' } },
-            { name: { contains: 'food', mode: 'insensitive' } },
-            { name: { contains: 'boarding', mode: 'insensitive' } },
-          ],
-        },
-      });
-
-      // Delete existing AdditionalFeeStudent records
-      await prisma.additionalFeeStudent.deleteMany({
-        where: {
-          studentId: student.id,
-        },
-      });
-
-      // Create additional fee student records based on matched fees
-      const additionalFeeStudents = fees.map((fee) => {
-        return prisma.additionalFeeStudent.create({
-          data: {
-            studentId: student.id,
-            additionalFeeId: fee.id,
-          },
-        });
-      });
-
-      // Wait for all additional fee student records to be created
-      await Promise.all(additionalFeeStudents);
-
-      // Calculate the fee amounts
-      let busFee = 0;
-      let foodFee = 0;
-      let boardingFee = 0;
-
-      for (const fee of fees) {
-        if (fee.name.toLowerCase().includes('bus')) {
-          busFee += Number(fee.amount);
-        }
-        if (fee.name.toLowerCase().includes('food')) {
-          foodFee += Number(fee.amount);
-        }
-        if (fee.name.toLowerCase().includes('boarding')) {
-          boardingFee += Number(fee.amount);
-        }
-      }
-
-      // Delete existing StudentTermFee records
-      await prisma.studentTermFee.deleteMany({
-        where: {
-          studentId: student.id,
-        },
-      });
-      // Create the student term fee record
-      const studentTermFee = await prisma.studentTermFee.create({
-        data: {
-          studentId: student.id,
-          classId: data.classId,
-          bus_fee: additionalPayments.bus_fee ? busFee : 0,
-          food_fee: additionalPayments.food_fee ? foodFee : 0,
-          boarding_fee: additionalPayments.boarding_fee ? boardingFee : 0,
-          term_one_fee: classInfo.term_1 ?? 0,
-          term_one_paid: additionalPayments.term_one_paid ?? 0,
-          term_one_balance:
-            Number(classInfo.term_1) +
-              (additionalPayments.bus_fee ? busFee : 0) +
-              (additionalPayments.food_fee ? foodFee : 0) +
-              (additionalPayments.boarding_fee ? boardingFee : 0) -
-              (additionalPayments.term_one_paid ?? 0) ?? 0,
-          term_two_fee: classInfo.term_2 ?? 0,
-          term_two_paid: additionalPayments.term_two_paid ?? 0,
-          term_two_balance:
-            Number(classInfo.term_2) +
-              (additionalPayments.bus_fee ? busFee : 0) +
-              (additionalPayments.food_fee ? foodFee : 0) +
-              (additionalPayments.boarding_fee ? boardingFee : 0) -
-              (additionalPayments.term_two_paid ?? 0) ?? 0,
-          term_three_fee: classInfo.term_3 ?? 0,
-          term_three_paid: additionalPayments.term_three_paid ?? 0,
-          term_three_balance:
-            Number(classInfo.term_3) +
-              (additionalPayments.bus_fee ? busFee : 0) +
-              (additionalPayments.food_fee ? foodFee : 0) +
-              (additionalPayments.boarding_fee ? boardingFee : 0) -
-              (additionalPayments.term_three_paid ?? 0) ?? 0,
-        },
-      });
-
-      res.status(200).json({
-        student,
-
-        busFee,
-        foodFee,
-        boardingFee,
-
-        studentTermFee,
-      });
+      res.status(202).json(student);
     } catch (error) {
       next(error);
+      return res.status(404).json({ message: 'Student not found' });
     }
   }
 );
-
-// get all students with pagination
+      
+      // get all students with pagination
 router.get(
   '/students',
   async (req: Request, res: Response, next: NextFunction) => {
